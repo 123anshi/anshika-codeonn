26b9b9cae516919b2f5ec4b70043e58d442b9aa6
123anshi <157716478+123anshi@users.noreply.github.com>
2024-06-16 13:48:33 +0000

    configure done

--- learn/basic.js
+++ learn/basic.js
@@ -1,1 +1,22 @@
-console.log("anshika")
+// // variable declare
+// const accountId = 10 //fixed not changed
+// let accountEmail = "singhgooggle.com"
+// var accountPass = "12345"//not used mainly curly braces(scope)not work javascript then use let(do not use var)
+// accountCity = "jaipur"
+// let accountState;//give undefined after print
+// //block scope and functional scope(var not use)
+
+// console.table([accountId ,accountEmail,accountPass ,accountCity,accountState] );
+
+//DATA TYPE:-number,bigint,string,boolean,null - standalone value(nill is representation of empty value),undefined(not value assign)
+//symbol(unique react js),object,
+
+"use strict";//treat all js code as newer version
+// alert("hello") //popup
+ //code readability 
+//  let state= null;
+//  console.log(typeof null); //output -object(some one saythis is language error but null typeis object)
+
+ 
+
+
