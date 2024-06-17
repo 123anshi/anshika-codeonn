4efc151b36630a96ab0f560b0b22bf5f8411d159
123anshi <157716478+123anshi@users.noreply.github.com>
2024-06-17 19:58:55 +0000

    configure done

--- learn/basic.js
+++ learn/basic.js
@@ -1,3 +1,5 @@
+//** DAY 1*/
+
 // // variable declare
 // const accountId = 10 //fixed not changed
 // let accountEmail = "singhgooggle.com"
@@ -17,6 +19,47 @@
 //  let state= null;
 //  console.log(typeof null); //output -object(some one saythis is language error but null typeis object)
 
- 
+//** DAY 2*/
+
+//data type conversion.
+// let score;
+// console.log(typeof(score))
+// //  const {score} =req.body
+// let value = Number(score)
+// console.log(typeof(value));
+// console.log(value);
+
+// coversion of data type
+
+// if No. convert in to 33 = output is easily covert into no.
+// string= nan
+// let logged;
+// let booleanlogged=Boolean(logged)
+// console.log(booleanlogged);
+// empty=false
+// let sumNo=33;
+// let stringsNo = String(sumNo);
+// console.log(typeof stringsNo); //output= string
 
 
+
+//**** operations****
+// let value = 3;
+// let negValu = -value;
+// console.log(negValu) //o/p= -3
+// console.log(2*2);reamainder percentage symbol is important
+// to string add
+// let str1;
+// let str2;
+// let str3=str1+str2;
+// console.log(str3)
+// console.log("1"+2+2) //op=122
+// console.log(1+2+"3") //op=33
+// based on preference
+// console.log(+true)//not prefer to this tricky conversion
+// better code, clean code, easy to ready  and optimization is our aim
+// prefix and postfix study through mdn refrence
+// comparison= console.log(2>1) op = true
+// console.log("2">1) //dont campare different data type(type scripter not allow this)javascript covert this firstlly into no.
+//comparison and equality is different
+// === strict check also check dta type
